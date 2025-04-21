edecea13c0946ef10a25c89e73f95ee4a22d55eb
Gourav Jain <gourav.jain@intimetec.com>
2025-04-21 20:24:23 +0530

    Assignment of chapter 5

---
+++ Chapter5/question1.java
@@ -0,0 +1,23 @@
+Class Employee {
+    string name;
+    int age;
+    float salary;
+
+    public : string getName();
+
+    void setName(string name);
+
+    int getAge();
+
+    void setAge(int age);
+
+    float getSalary();
+
+    void setSalary(float salary);
+
+};
+
+Employee employee;
+
+//Here 'employee' is data structure as it doesn't have any functions to manipulate data 
+// and it's also not following abstraction
